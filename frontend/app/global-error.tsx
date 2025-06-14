e36573830d7a4c1822b8fe90106eda9ebe7bac28
Ganesh Darapureddi <ganesh.darapureddi@pal.tech>
2025-06-14 23:14:49 +0530

    added the home page with tailwind CSS

--- frontend/app/global-error.tsx
+++ frontend/app/global-error.tsx
@@ -12,15 +12,17 @@ export default function GlobalError({
 }) {
   useEffect(() => {
     console.error('Global error:', error)
-  }, [error])
+  }, [error]) 
 
   return (
-    <div className={styles.container}>
-      <h2 className={styles.heading}>Something went wrong globally!</h2>
-      <p className={styles.message}>{error.message}</p>
-      <button onClick={() => reset()} className={styles.button}>
-        Try again
-      </button>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="border-none  p-10 bg-white rounded-4xl shadow-2xl max-w-4xl">
+        <h2 className="text-xl text-red-700 font-bold text-center mb-5">Something went wrong globally!</h2>
+        <p className="mb-4   text-xl font-serif">{error.message}</p>
+        <button onClick={() => reset()} className="block mx-auto px-4 py-2 bg-red-600 text-white text-center rounded-xl  hover:bg-red-700">
+          Try again
+        </button>
+      </div>
     </div>
   )
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,46 +1,39 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import '@/app/page.css';
+
 
 export default function IntroductionPage() {
   return (
-    <main className="intro-container">
-      <section className="intro-hero">
-        <div className="intro-text">
-          <h1>Welcome to <span>HealthReserve</span></h1>
-          <p>
-            HealthReserve is your one-stop platform to book appointments with top doctors,
-            manage your health schedule, and stay on top of your wellness—all in one place.
-          </p>
-          <Link href="/dashboard/doctors" className="intro-button">Explore Doctors</Link>
+    <main className="p-12">
+    <section className="min-h-screen flex flex-col flex-wrap justify-center items-center md:flex-row ">
+      <div className="md:w-1/2">
+        <h1 className='font-bold text-3xl  font-sans mb-10'>Welcome to <span className="text-6xl  font-normal bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-purple-900">Health Reserve</span></h1>
+        <p className='w-3/4 text-gray-500 mb-7 font-serif line-'>
+          HealthReserve is your all-in-one platform for managing health. <br />
+          Book appointments with top-rated doctors anytime, anywhere. <br />
+          Stay organized with your personalized health schedule. <br />
+          Access all your wellness needs in a single,  simple dashboard. <br />
+          Take control of your health journey—seamlessly and securely. <br />
+        </p>
+        <div className='gap-4 flex mb-4'>
+          <Link
+            href="/dashboard/doctors"
+            className="text-white bg-purple-900 border-2 border-purple-950 p-1 px-3 rounded-sm hover:bg-white hover:text-purple-950 transition"            >
+            Explore Doctors
+          </Link>  
+          <Link href="/dashboard/appointment" className="border-2 border-purple-900 text-purple-950 p-1 px-3 rounded-sm hover:bg-purple-900 hover:text-white">Make an Appointment</Link>
         </div>
-        <div className="intro-image">
-          <Image
-            src="/doctor-image.png"
-            width={400}
-            height={400}
-            alt="Doctor Image"
-          />
-        </div>
-      </section>
-
-      <section className="features-section">
-        <h2>Why Choose HealthReserve?</h2>
-        <div className="features-grid">
-          <div className="feature-card">
-            <h3>Easy Booking</h3>
-            <p>Find available doctors and book an appointment in just a few clicks.</p>
-          </div>
-          <div className="feature-card">
-            <h3>Smart Scheduling</h3>
-            <p>Manage your upcoming visits effortlessly and Save your time</p>
-          </div>
-          <div className="feature-card">
-            <h3>Secure Platform</h3>
-            <p>Your health data is encrypted and stored securely with care.</p>
-          </div>
-        </div>
-      </section>
-    </main>
+      </div>
+      <div className="md:w-1/2 shadow-2xl rounded-2xl ">
+        <Image
+          src="/Intro-Doctor.png"
+          width={900}
+          height={900}
+          alt="Intro Image"
+          className='rounded-2xl'
+        />
+      </div>
+    </section>
+  </main>
   );
 }
