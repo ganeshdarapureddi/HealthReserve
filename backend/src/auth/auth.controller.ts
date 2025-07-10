e6181f0c3c1f9ce1c586a4ad735645485ee65609
Ganesh Darapureddi <ganesh.darapureddi@pal.tech>
2025-07-10 16:47:26 +0530

    made session time to 1 hr

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -34,7 +34,7 @@ export class AuthController {
 
     const accessToken = await this.jwtService.signAsync(payload, {
       secret: process.env.JWT_SECRET,
-      expiresIn: '30s',
+      expiresIn: '1hr',
     });
 
     const refreshToken = await this.jwtService.signAsync(payload, {
@@ -124,7 +124,7 @@ export class AuthController {
         { sub: payload.sub, role: payload.role },
         {
           secret: process.env.JWT_SECRET,
-          expiresIn: '30s',
+          expiresIn: '1hr',
         },
       );
   
